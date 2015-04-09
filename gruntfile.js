b2d98ec4b66eccd7dfec9e8f52528f68569841be
Daniel Stern <daniel@danielstern.ca>
2015-04-09 10:53:47 -0400

    :facepunch:

--- gruntfile.js
+++ gruntfile.js
@@ -1,27 +1,19 @@
-// Generated on 2014-08-11 using
-// generator-webapp 0.5.0-1
 'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// If you want to recursively match all subfolders, use:
-// 'test/spec/**/*.js'
 
 module.exports = function(grunt) {
 
-    // Load grunt tasks automatically
-    // require('grunt-typescript')(grunt);
     grunt.loadNpmTasks('grunt-typescript');
 
     // Define the configuration for all the tasks
     grunt.initConfig({     
 
-        // typescript yeah!
         typescript: {
-            base: {
-                src: ['scripts/**/{,*/}*.ts'],
-                dest: '.tmp/scripts/',
+            main: {
+                src: ['typescript/**/{,*/}*.ts'],
+                dest: 'javascript/main.js',
+
+
+                /* Options are, well... optional. */
                 options: {
                     module: 'amd', //or commonjs
                     target: 'es5', //or es3
